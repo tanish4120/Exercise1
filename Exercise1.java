e28e8e51e82f87008242e809b5f68d24aab8f4ea
tanish4120 <124870021+tanish4120@users.noreply.github.com>
2023-03-16 13:54:15 +0530

    Update and rename Exercise1 to Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Exercise1 {
+
+    
+  public static void main(String[] args)
+    {
+        
+        
+            System.out.println("Number is zero");
+        
+    }
+
+}
